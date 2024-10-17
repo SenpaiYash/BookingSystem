35cfd82e03168318769cb1f3c5b370b3d0fd8a32
Senpai Yash <Senpai Yash@LAPTOP-C19SPR1M>
2024-10-18 03:03:42 +0400

    Changes in Calender

--- External Services/CalendarSyncService.cs
+++ External Services/CalendarSyncService.cs
@@ -1,11 +1,22 @@
 ﻿using BookingSystem.Models;
+using Google.Apis.Calendar.v3;
+using Microsoft.Graph;
 
 namespace BookingSystem.External_Services
 {
     public class CalendarSyncService : ICalendarSyncService
     {
+        private readonly GraphServiceClient _graphClient;
+        private readonly CalendarService _calendarService;
+
+        public CalendarSyncService(GraphServiceClient graphService,CalendarService calendarService) 
+        { 
+            _graphClient = graphService;
+            _calendarService = calendarService;
+        }
         public Task ExchangeCalenderAsync(BookingModel booking)
-        {
+        {   
+
             return Task.CompletedTask;
         }
 
